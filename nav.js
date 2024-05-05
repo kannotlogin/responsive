18c5095495999f97db8503772ebcadba6494b331
kannotlogin <98953750+kannotlogin@users.noreply.github.com>
2024-05-05 19:57:56 +0200

    Add files via upload

--- nav.js
+++ nav.js
@@ -25,3 +25,19 @@ toggle.addEventListener("click", () => {
 });
 
 toggle.addEventListener("click", () => toggle.classList.toggle("active"));
+
+//Animation
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show')
+        } else {
+            entry.target.classList.remove('show')
+        }
+    })
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
